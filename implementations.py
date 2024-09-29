9eaed006cac0ac37ff02b0b37c48bf84bde12ea3
Matthias Wyss <matthiaswyss74@gmail.com>
2024-09-29 10:33:00 +0200

    adding functions from ex03

--- implementations.py
+++ implementations.py
@@ -48,10 +48,11 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     ws = [initial_w]
     losses = []
     w = initial_w
+    
     for n_iter in range(max_iters):
         gradient = compute_gradient(y, tx, w)
-        loss = mse(y, tx, w)
         w = w - gamma * gradient
+        loss = mse(y, tx, w)
 
         # store w and loss
         ws.append(w)
@@ -80,6 +81,66 @@ def compute_stoch_gradient(y, tx, w):
     B = np.shape(y)[0]
     e = y - tx @ w
     return -1/B * tx.T @ e
+
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+
+    Example:
+
+     Number of batches = 9
+
+     Batch size = 7                              Remainder = 3
+     v     v                                         v v
+    |-------|-------|-------|-------|-------|-------|---|
+        0       7       14      21      28      35   max batches = 6
+
+    If shuffle is False, the returned batches are the ones started from the indexes:
+    0, 7, 14, 21, 28, 35, 0, 7, 14
+
+    If shuffle is True, the returned batches start in:
+    7, 28, 14, 35, 14, 0, 21, 28, 7
+
+    To prevent the remainder datapoints from ever being taken into account, each of the shuffled indexes is added a random amount
+    8, 28, 16, 38, 14, 0, 22, 28, 9
+
+    This way batches might overlap, but the returned batches are slightly more representative.
+
+    Disclaimer: To keep this function simple, individual datapoints are not shuffled. For a more random result consider using a batch_size of 1.
+
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)  # NUmber of data points.
+    batch_size = min(data_size, batch_size)  # Limit the possible size of the batch.
+    max_batches = int(
+        data_size / batch_size
+    )  # The maximum amount of non-overlapping batches that can be extracted from the data.
+    remainder = (
+        data_size - max_batches * batch_size
+    )  # Points that would be excluded if no overlap is allowed.
+
+    if shuffle:
+        # Generate an array of indexes indicating the start of each batch
+        idxs = np.random.randint(max_batches, size=num_batches) * batch_size
+        if remainder != 0:
+            # Add an random offset to the start of each batch to eventually consider the remainder points
+            idxs += np.random.randint(remainder + 1, size=num_batches)
+    else:
+        # If no shuffle is done, the array of indexes is circular.
+        idxs = np.array([i % max_batches for i in range(num_batches)]) * batch_size
+
+    for start in idxs:
+        start_index = start  # The first data point of the batch
+        end_index = (
+            start_index + batch_size
+        )  # The first data point of the following batch
+        yield y[start_index:end_index], tx[start_index:end_index]
 
 
 def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma, batch_size=1):
@@ -123,7 +184,7 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma, batch_size=1):
 
 
 def least_squares(y, tx):
-    """Calculate the least squares solution.
+    """Calculate the least squares solution using QR decomposition if possible, or normal equations otherwise.
        returns mse, and optimal weights.
 
     Args:
@@ -138,8 +199,40 @@ def least_squares(y, tx):
     (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
     """
     
-    w = np.linalg.solve(tx.T @ tx, tx.T @ y)
+    try:
+        # Solve for w using the upper triangular matrix R
+        Q, R = np.linalg.qr(tx)
+        w = np.linalg.solve(R, Q.T @ y)
+    except:
+        # Solve for w using normal equations
+        w = np.linalg.solve(tx.T @ tx, tx.T @ y)
+    
     N = np.shape(y)[0]
     e = y - tx @ w
     mse = 1/(2*N) * e.T @ e
     return w, mse
+
+
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        lambda_: scalar.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 0)
+    array([ 0.21212121, -0.12121212])
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
+    array([0.03947092, 0.00319628])
+    """
+    
+    N, D = tx.shape
+    lambda_prime = 2 * N * lambda_
+    I = np.eye(D)
+    w = np.linalg.solve(tx.T @ tx + lambda_prime * I, tx.T @ y)
+    
+    return w
